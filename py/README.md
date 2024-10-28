9fbd848994e4c8f1fd519da64e1d6f5ab063a853
Carl Karsten <carl@personnelware.com>
2024-10-28 16:07:12 -0500

    add command line parameters.

--- py/README.md
+++ py/README.md
@@ -7,6 +7,28 @@ git clone https://github.com/realthunder/kicad_parser
 ```
 
 # Usage
- * in Kicad: Schematic Editor, File/Export/Netlist, (Export Netlist) button, filename: pj.net, (Save) button.
- * python pj.py
+ * in Kicad: Schematic Editor
+ * File/Export/Netlist
+ * click (Export Netlist) button
+ * filename: pj.net
+ * click (Save) button.
+
+```
+$ python pj.py 0 6
+TMS | BCM27 | 13 | 9 | GPIO6
+TDI | BCM26 | 37 | 10 | GPIO7
+TCK | BCM25 | 22 | 11 | GPIO8
+TDO | BCM24 | 18 | 12 | GPIO9
+RTCK | BCM23 | 16 | 14 | GPIO10
+TRST | BCM22 | 15 | 15 | GPIO11
+
+$ python pj.py 6 8
+pico TX | BCM15_RXD | 10 | 1 | GPIO0
+pico RX | BCM14_TXD | 8 | 2 | GPIO1
+
+$ python pj.py 8 10
+x1 | BCM4_GPCLK0 | 7 | 16 | GPIO12
+x2 | BCM3_SCL | 5 | 17 | GPIO13
+```
+
 
--- py/pj.py
+++ py/pj.py
@@ -1,36 +1,89 @@
 # pj.py
 # dump some netmasky stuff to confirm I wired things up the way I wanted.
 
+import argparse
+
 from operator import itemgetter, attrgetter, methodcaller
 from pprint import pprint
 
 from kicad_parser import KicadPCB
 
 
-net = KicadPCB.load('../project/pj.net')
+def get_net(filename, start,end, verbose):
 
-l=[]
+    net = KicadPCB.load(filename)
 
-for i in range(8,10):
+    l=[]
 
-    n = net.nets.net[i]
-    d = {
-            'code' : eval(n.code),
-            'name' : eval(n.name)[1:],
-            'pico' : { 'name': eval(n.node[0].pinfunction), 'pin' : eval(n.node[0].pin) },
-            'index' : int(eval(n.node[0].pin)),
-            'pi' : { 'name': eval(n.node[1].pinfunction), 'pin' : eval(n.node[1].pin) },
-            }
-    # pprint(d)
-    l.append(d)
+    for i in range(start,end):
+
+        n = net.nets.net[i]
+        d = {
+                'code' : eval(n.code),
+                'name' : eval(n.name)[1:],
+                'pico' : { 'name': eval(n.node[0].pinfunction), 'pin' : eval(n.node[0].pin) },
+                'index' : int(eval(n.node[0].pin)),
+                'pi' : { 'name': eval(n.node[1].pinfunction), 'pin' : eval(n.node[1].pin) },
+                }
+        if verbose: pprint(d)
+        l.append(d)
+
+        if verbose: pprint(l)
+
+    return l
 
 
-# pprint(l)
-l=sorted(l,key=itemgetter( 'index' ) )
+def sort_net(l, key):
+    l=sorted(l,key=itemgetter( key ) )
+    return l
 
-# pprint(l)
+def wiki_rows(l):
 
-for i in l:
-    # print(" | ".join([i['name'], i['pi']['name'][3:], i['pi']['pin'], i['pico']['pin'], i['pico']['name'][4:]]))
-    print(" | ".join([i['name'], i['pi']['name'], i['pi']['pin'], i['pico']['pin'], i['pico']['name']]))
+    for i in l:
+        # print(" | ".join([i['name'], i['pi']['name'][3:], i['pi']['pin'], i['pico']['pin'], i['pico']['name'][4:]]))
+        print(" | ".join([i['name'], i['pi']['name'], i['pi']['pin'], i['pico']['pin'], i['pico']['name']]))
+
+
+def get_args():
+
+    parser = argparse.ArgumentParser(
+            description="KiCad netlist thing")
+
+    parser.add_argument('range',
+            help='start end netlist range',
+            nargs='+',
+            type=int,
+            )
+
+    parser.add_argument('--filename',
+            help='input filename',
+            nargs='?',
+            default='../project/pj.net',
+            )
+
+    parser.add_argument('-k', '--key',
+            help='key to sort on',
+            default='index',
+            )
+
+    parser.add_argument('-t', '--test', help="run test(s)", action="store_true")
+    parser.add_argument('-v', '--verbose', action="store_true")
+
+    args = parser.parse_args()
+
+    return args
+
+
+def main():
+    args = get_args()
+
+    net = get_net(args.filename, args.range[0], args.range[1], args.verbose)
+    net = sort_net(net, args.key)
+
+    wiki_rows(net)
+
+
+if __name__=='__main__':
+    main()
+
 
